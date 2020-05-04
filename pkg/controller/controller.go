7746737fa58100e9b33ef3b1332e3f63e920ed5f
Amir Mofasser <amir.mofasser@gmail.com>
2020-05-04 10:02:55 +0200

    Add configuration through `Config` struct
    * Use synkas own configuration file config.yaml that can be passed in using a cmd flag
    * Each cluster has an instance of dynamic client

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"context"
 	"fmt"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -17,43 +18,46 @@ import (
 
 // Controller is a k8s controller implementation
 type Controller struct {
-	queue   workqueue.RateLimitingInterface
-	factory dynamicinformer.DynamicSharedInformerFactory
-	gvr     *schema.GroupVersionResource
-	indexer cache.Indexer
+	queue    workqueue.RateLimitingInterface
+	factory  dynamicinformer.DynamicSharedInformerFactory
+	gvr      *schema.GroupVersionResource
+	indexer  cache.Indexer
+	clusters []Cluster
+	config   *Config
 }
 
-// New creates a new instance of controller
-func New(client dynamic.Interface, gvr *schema.GroupVersionResource) *Controller {
+// New creates a new instance of controller for the given GroupVersionResource
+// See https://godoc.org/k8s.io/apimachinery/pkg/runtime/schema#GroupVersionResource for more information
+func New(client dynamic.Interface, config *Config, gvr *schema.GroupVersionResource) *Controller {
 	return &Controller{
 		factory: dynamicinformer.NewFilteredDynamicSharedInformerFactory(client, 0, v1.NamespaceAll, nil),
 		queue:   workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "synka.io"),
 		gvr:     gvr,
+		config:  config,
 	}
 }
 
 // Run will set up the event handlers for types we are interested in, as well
-// as syncing informer caches and starting workers. It will block until stop channel
+// as syncing informer caches and starting workers. It will block until stopCh channel
 // is closed, at which point it will shutdown the workqueue and wait for
 // workers to finish processing their current work items.
-func (c *Controller) Run(stop chan struct{}) {
+func (c *Controller) Run(stopCh <-chan struct{}) {
 	defer runtime.HandleCrash()
 	defer c.queue.ShutDown()
-	klog.Infof("Starting controller %s", c.gvr.GroupVersion().String())
-
 	informer := c.factory.ForResource(*c.gvr)
 	c.indexer = informer.Informer().GetIndexer()
-	go c.startWatching(stop, informer.Informer())
+	go c.startWatching(stopCh, informer.Informer())
 
-	if !cache.WaitForCacheSync(stop, informer.Informer().HasSynced) {
+	if !cache.WaitForCacheSync(stopCh, informer.Informer().HasSynced) {
 		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
 		return
 	}
 
-	go wait.Until(c.runWorker, time.Second, stop)
+	go wait.Until(c.runWorker, time.Second, stopCh)
 
-	<-stop
-	klog.Infof("Stopping controller %s", c.gvr.GroupVersion().String())
+	klog.Infof("Started controller for %s", c.gvr.GroupResource().String())
+	<-stopCh
+	klog.Infof("Shutting down controller for %s", c.gvr.GroupResource().String())
 
 }
 
@@ -77,17 +81,48 @@ func (c *Controller) processNextItem() bool {
 
 func (c *Controller) syncToStdout(key string) error {
 	obj, exists, err := c.indexer.GetByKey(key)
+
 	if err != nil {
 		klog.Errorf("Fetching objects witch key %s from store failed with %v", key, err)
 		return err
 	}
 
 	u := obj.(*unstructured.Unstructured)
+
 	if !exists {
 		klog.Infof("Resource %s does not exists anymore", key)
 	} else {
-		klog.Infof("Sync %s/%s: %s", u.GetAPIVersion(), u.GetKind(), u.GetName())
+
+		for _, cluster := range c.config.Clusters {
+
+			// Get a client for the GroupVersionResource
+			client, err := cluster.GetClient(c.gvr)
+			if err != nil {
+				return err
+			}
+
+			// Only go any further if object is annotated properly
+			a := u.GetAnnotations()
+			if _, ok := a["synka.io/sync"]; ok {
+				if a["synka.io/sync"] == "true" {
+
+					klog.V(4).Infof("Sync %s/%s: %s", u.GetAPIVersion(), u.GetKind(), u.GetName())
+
+					// Remove any immutable fields
+					u.Object["metadata"].(map[string]interface{})["resourceVersion"] = ""
+
+					// Create the resource in target cluster using the cluters's dynamic client
+					result, err := client.Resource(*c.gvr).Namespace(u.GetNamespace()).Create(context.Background(), u, v1.CreateOptions{})
+					if err != nil {
+						return err
+					}
+					klog.V(4).Infof("Created %s/%s on %s", result.GetKind(), result.GetName(), cluster.Name)
+
+				}
+			}
+		}
 	}
+
 	return nil
 }
 
